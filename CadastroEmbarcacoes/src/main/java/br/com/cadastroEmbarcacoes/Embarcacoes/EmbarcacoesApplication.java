cda3c2f0d7d46ecf13c6b9e264e52051c258dde0
Lucas de Figueiredo Aguiar <37455976+sendrak@users.noreply.github.com>
2021-09-05 18:47:04 -0300

    Criação do Projeto.

---
+++ CadastroEmbarcacoes/src/main/java/br/com/cadastroEmbarcacoes/Embarcacoes/EmbarcacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastroEmbarcacoes.Embarcacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbarcacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbarcacoesApplication.class, args);
+	}
+
+}
---
+++ CadastroEmbarcacoes/src/test/java/br/com/cadastroEmbarcacoes/Embarcacoes/EmbarcacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadastroEmbarcacoes.Embarcacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbarcacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
